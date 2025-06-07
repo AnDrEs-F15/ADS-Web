5479e3bce939bc4b158e61d24414211f3ac8feb1
Andres Felipe Pipicano Villa <1005828561@icesi.edu.co>
2025-06-07 15:21:39 -0500

    Proyecto Analisis y diseño de software

---
+++ src/views/pages/auth-forms/hooks/useAuthLogin.js
@@ -0,0 +1,34 @@
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { postAuthLogin } from '../../../../api/auth';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+import { login } from '../../../../store/slice/authSlice';
+export const useAuthLogin = () => {
+  const dispatch = useDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log('Datos del login:', data);
+    //fetchAuthLogin.mutate(data);
+    toast.success('Ingreso exitoso');
+    dispatch(login({ token: 'TOKEN_MOCK' }));
+    // Aquí puedes hacer el dispatch o llamada a tu API
+  });
+
+  const fetchAuthLogin = useMutation({
+    mutationFn: postAuthLogin,
+    onSuccess: (data) => {
+      toast.success('Ingreso exitoso');
+      dispatch(login({ token: data?.token }));
+    },
+    onError: () => toast.error('Verifique las credenciales por favor')
+  });
+
+  return { onSubmit, register, errors };
+};
